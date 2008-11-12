7ff8cc751f6edea54de1fb0f6e4af5f10cd5c917
Fredrik Vraalsen <fredriv@badwolf.(none)>
2008-11-12 19:56:55 +0100

    get contact list from EMS

--- src/main/scala/no/java/submitit/snippet/HelloWorld.scala
+++ src/main/scala/no/java/submitit/snippet/HelloWorld.scala
@@ -1,6 +1,26 @@
 package no.java.submitit.snippet
 
+import no.java.ems.client._
+import no.java.ems.domain._
+import _root_.java.util.List
+import _root_.scala.collection.jcl.Conversions._
+
 class HelloWorld {
-  def howdy = <span>Welcome to submitit at {new _root_.java.util.Date}</span>
+  def url = "http://localhost:3000/ems"
+  def service = new RestEmsService(url)
+  
+  def contacts = service.getContacts().toList
+  
+  def howdy = <div>
+  <table border="1">
+  <tr><th>Id</th><th>Name</th><th>E-mail</th></tr>
+  {contacts.map(contact => 
+    <tr>
+    <td>{contact.getId}</td>
+      <td>{contact.getName}</td>
+      <td>{contact.getEmailAddresses.map(email => email.getEmailAddress).mkString(" FOOBAR ")}</td>
+      </tr>)}
+  </table>
+  </div>
 }
 
